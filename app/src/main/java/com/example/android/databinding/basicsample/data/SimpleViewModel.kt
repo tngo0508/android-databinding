1c0679c3edd22e29fbc26a73061a3c10c77c2c09
thomas <tngo0508@gmail.com>
2018-11-29 01:51:15 -0800

    add observing data

--- app/src/main/java/com/example/android/databinding/basicsample/data/SimpleViewModel.kt
+++ app/src/main/java/com/example/android/databinding/basicsample/data/SimpleViewModel.kt
@@ -17,6 +17,8 @@
 package com.example.android.databinding.basicsample.data
 
 import android.arch.lifecycle.ViewModel
+import android.databinding.ObservableField
+import android.databinding.ObservableInt
 
 /**
  * A simple VM for [com.example.android.databinding.basicsample.ui.PlainOldActivity].
@@ -24,27 +26,21 @@ import android.arch.lifecycle.ViewModel
 class SimpleViewModel : ViewModel() {
     val name = "Grace"
     val lastName = "Hopper"
-    var likes = 0
-        private set // This is to prevent external modification of the variable.
+    val likes = ObservableInt()
+    val popularity = ObservableField<Popularity>(Popularity.NORMAL)
 
-    /**
-     * Increments the number of likes.
-     */
     fun onLike() {
-        likes++
-    }
+        likes.set(likes.get() + 1)
 
-    /**
-     * Returns popularity in buckets: [Popularity.NORMAL], [Popularity.POPULAR] or [Popularity.STAR]
-     */
-    val popularity: Popularity
-        get() {
-            return when {
-                likes > 9 -> Popularity.STAR
-                likes > 4 -> Popularity.POPULAR
+        popularity.set(likes.get().let {
+            when {
+                it > 9 -> Popularity.STAR
+                it > 4 -> Popularity.POPULAR
                 else -> Popularity.NORMAL
             }
-        }
+        })
+    }
+
 }
 
 enum class Popularity {
